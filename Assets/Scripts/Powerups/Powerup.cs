48e15dc20c486fb87d22157483605a0453eb2a59
Lorenzo Pesce <lorenzo.pesce.1996@gmail.com>
2025-04-21 21:31:53 +0200

    chore: improved pickup system

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public abstract class Powerup : MonoBehaviour, Pickupable
 {
+    [Header("Rotation and bobbing")]
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 90f, 0f);
+    [SerializeField] private float bobbingAmplitude = 0.25f;
+    [SerializeField] private float bobbingFrequency = 1f;
+    [SerializeField] private GameObject visualObject;
+    
+    [Header("Sound Effects")]
+    [SerializeField] private AudioClip pickupSound;
+    [SerializeField] private float volume = 1f;
+    
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = visualObject.transform.position;
+    }
+
+    void Update()
+    {
+        visualObject.transform.Rotate(rotationSpeed * Time.deltaTime);
+
+        float newY = startPos.y + Mathf.Sin(Time.time * bobbingFrequency) * bobbingAmplitude;
+        visualObject.transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            SoundFXManager.instance.PlaySoundFxClip(pickupSound, transform, volume);
             Pickup();
+        }
     }
 
     public virtual void Pickup()
