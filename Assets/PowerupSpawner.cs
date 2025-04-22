00f615266ab61e1c4fc947b18cdf44f724446581
Lorenzo Pesce <lorenzo.pesce.1996@gmail.com>
2025-04-22 12:06:24 +0200

    feat: added random powerup spawner instead of static ones

---
+++ Assets/PowerupSpawner.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    [SerializeField] private List<SpawnablePowerup> powerups;
+    
+    public void SpawnRandomPowerup()
+    {
+        if (powerups == null || powerups.Count == 0)
+        {
+            Debug.LogWarning("No powerups available to spawn.");
+            return;
+        }
+
+        int totalWeight = 0;
+        foreach (var powerup in powerups)
+        {
+            totalWeight += powerup.weight;
+        }
+
+        int randomValue = Random.Range(0, totalWeight);
+        int currentSum = 0;
+
+        foreach (var powerup in powerups)
+        {
+            currentSum += powerup.weight;
+            if (randomValue < currentSum)
+            {
+                Instantiate(powerup.powerupPrefab, new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z), Quaternion.identity);
+                return;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        SpawnRandomPowerup();
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
+
+[System.Serializable]
+public class SpawnablePowerup
+{
+    public GameObject powerupPrefab;
+    public int weight;
+}
